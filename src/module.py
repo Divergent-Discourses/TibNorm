ac2ed61c9e960b35da1c720888675090f1002615
ykyogoku <kyogoku11@gmail.com>
2024-02-07 16:16:13 +0100

    change output format

--- src/module.py
+++ src/module.py
@@ -10,11 +10,11 @@ def load_tables(config):
     files = [file for file in glob.glob(pattern, recursive=True) if os.path.isfile(file)]
     for file in files:
         file_name = os.path.basename(file).split('.')[0]
+        df = pd.read_csv(file, sep='\t', escapechar='\\', index_col=None)
         if file_name == 'abbreviations':
-            df = pd.read_csv(file, sep='\t', escapechar='\\', index_col=None)
             tables[file_name] = df[df['flag'] == flag]
         else:
-            tables[file_name] = pd.read_csv(file, sep='\t', escapechar='\\', index_col=None)
+            tables[file_name] = df
 
     return tables
 
@@ -23,41 +23,50 @@ def load_texts(config):
     path = config['paths']['text_path']
     documents = os.listdir(path)
     for doc in documents:
-        texts[doc] = str()
+        texts[doc] = {}
+        # texts[doc] = str()
         pattern = os.path.join(path , doc, '**', '*.txt')
         files = [file for file in glob.glob(pattern, recursive=True) if os.path.isfile(file)]
         for file in files:
             file_name = os.path.basename(file).split('.')[0]
-            texts[doc] += open(file).read()
+            texts[doc][file_name] = open(file).read()
+        #     texts[doc] += open(file).read()
     return texts
 
 def norm_abbreviation(texts, tables):
     text_norm = {}
     table = tables['abbreviations'].set_index('transcription')['normalisation'].to_dict()
-    for doc, text in texts.items():
-        for key, value in table.items():
-            text = text.replace(key, value)
-        text_norm[doc] = text
+    for doc in texts.keys():
+        text_norm[doc] = {}
+    # for doc, text in texts.items():
+        for file_name, text in texts[doc].items():
+            for key, value in table.items():
+                text = text.replace(key, value)
+            text_norm[doc][file_name] = text
 
     return text_norm
 
 def norm_table1(texts, tables):
     text_norm = {}
     table = tables['table1'].set_index('transcription')['normalisation'].to_dict()
-    for doc, text in texts.items():
-        for key, value in table.items():
-            text = text.replace(key, value)
-        text_norm[doc] = text
+    for doc in texts.keys():
+        text_norm[doc] = {}
+        for file_name, text in texts[doc].items():
+            for key, value in table.items():
+                text = text.replace(key, value)
+            text_norm[doc][file_name] = text
 
     return text_norm
 
 def norm_table2(texts, tables):
     text_norm = {}
     table = tables['table2'].set_index('transcription')['normalisation'].to_dict()
-    for doc, text in texts.items():
-        for key, value in table.items():
-            text = re.sub(key, value, text)
-        text_norm[doc] = text
+    for doc in texts.keys():
+        text_norm[doc] = {}
+        for file_name, text in texts[doc].items():
+            for key, value in table.items():
+                text = re.sub(key, value, text)
+            text_norm[doc][file_name] = text
 
     return text_norm
 
@@ -66,26 +75,28 @@ def norm_table3(texts, tables):
     table = {}
     for index, row in tables['table3'].iterrows():
         table[row['transcription']] = (row['normalisation'], row['exception'], row['exc_len'])
-    for doc, text in texts.items():
-        text_list = list(text)
-        for key, value in table.items():
-            exception = re.compile(value[1])
-            exc_len = value[2]
-            for i in range(len(text_list)):
-                pos_end = i + len(key)
-                start = 0 if i - exc_len <= 0 else i - exc_len
-                end = len(text_list) -1 if pos_end + exc_len > len(text_list) else pos_end + exc_len
-                str_range = ''.join(text_list[start:end])
-                if len(key) > 1:
-                    if text_list[i:pos_end] == list(key):
-                        if not bool(exception.search(str_range)):
-                            text_list[i:pos_end] = [value[0]] + [''] * (pos_end - i - 1)
-                else:
-                    if text_list[i] == key:
-                        if not bool(exception.search(str_range)):
-                            text_list[i] = value[0]
+    for doc in texts.keys():
+        text_norm[doc] = {}
+        for file_name, text in texts[doc].items():
+            text_list = list(text)
+            for key, value in table.items():
+                exception = re.compile(value[1])
+                exc_len = value[2]
+                for i in range(len(text_list)):
+                    pos_end = i + len(key)
+                    start = 0 if i - exc_len <= 0 else i - exc_len
+                    end = len(text_list) -1 if pos_end + exc_len > len(text_list) else pos_end + exc_len
+                    str_range = ''.join(text_list[start:end])
+                    if len(key) > 1:
+                        if text_list[i:pos_end] == list(key):
+                            if not bool(exception.search(str_range)):
+                                text_list[i:pos_end] = [value[0]] + [''] * (pos_end - i - 1)
+                    else:
+                        if text_list[i] == key:
+                            if not bool(exception.search(str_range)):
+                                text_list[i] = value[0]
 
-        text_norm[doc] = ''.join(text_list)
+            text_norm[doc][file_name] = ''.join(text_list)
 
     return text_norm
 
@@ -106,7 +117,11 @@ def normalisation(texts, tables):
     return text_norm
 
 def export_text(config, text_norm):
-    for key, value in text_norm.items():
-        output_path = config['paths']['output_path'] + key + '.txt'
-        with open(output_path, 'w') as file:
-            file.write(value)
+    for doc in text_norm.keys():
+        output_dir = config['paths']['output_path'] + doc + '/'
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        for key, value in text_norm[doc].items():
+            output_path = output_dir + key + '.txt'
+            with open(output_path, 'w') as file:
+                file.write(value)
